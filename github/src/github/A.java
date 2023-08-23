ec3f3479dc4a04f1441ca37b56a7c7e7104adfa8
Shrinath6297 <143004844+Shrinath6297@users.noreply.github.com>
2023-08-23 20:10:00 +0530

    Update A.java

--- github/src/github/A.java
+++ github/src/github/A.java
@@ -5,6 +5,7 @@ public class A {
 	public void m1() {
 		
 		System.out.println("welcome");
+		Sysytem.out.println("hii");
 	}
 public static void main(String[] args) {
 	System.out.println("hello world");
