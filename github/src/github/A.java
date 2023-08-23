94809a3d55f6dc0bde2b678c0c4bc656abd3cfd7
Shri <Shri@DESKTOP-9HCNCKU>
2023-08-23 20:15:58 +0530

    shri

--- github/src/github/A.java
+++ github/src/github/A.java
@@ -5,6 +5,13 @@ public class A {
 	public void m1() {
 		
 		System.out.println("welcome");
+		
+	}
+	public void m2()
+	{
+		
+		System.out.println("shri");
+		
 	}
 public static void main(String[] args) {
 	System.out.println("hello world");
