1b675351647ea04e0a8fdcec72cf583d66227f49
Shri <Shri@DESKTOP-9HCNCKU>
2023-08-23 20:05:56 +0530

    By shri

--- github/src/github/A.java
+++ github/src/github/A.java
@@ -1,6 +1,11 @@
 package github;
 
 public class A {
+	
+	public void m1() {
+		
+		System.out.println("welcome");
+	}
 public static void main(String[] args) {
 	System.out.println("hello world");
 }
